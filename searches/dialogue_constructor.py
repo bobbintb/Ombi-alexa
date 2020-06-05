0f085e1592a9475f0643b33111a0dbca7b8162c3
bobbintb <bobbintb@gmail.com>
2020-06-05 12:46:23 -0600

    Update dialogue_constructor.py

--- searches/dialogue_constructor.py
+++ searches/dialogue_constructor.py
@@ -5,10 +5,13 @@ def construct(search_result):
     dialogue = f'There are {len(search_result)} results named {search_result[0]["title"]}.'
     print(dialogue)
     for i, match in enumerate(search_result):
-        dialogue += f' {i+1}. Released in {match["year"]} and starring {_get_cast(match)}.'
-    dialogue += ' Which one?'
+        dialogue += f' {i+1}. Released in {match["year"]} and starring {get_cast(match)}.'
+    #dialogue += ' Which one?'
     return dialogue
 
-def _get_cast(match):
+def get_cast(match):
     search = tmdb.Movies(match['id']).credits()
-    return search['cast'][0]['name']
+    if search['cast']:
+        return search['cast'][0]['name']
+    else:
+        return "an unknown actor"
